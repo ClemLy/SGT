efce208e2c2cd909a8209a1241a4b8e014e5db28
ClemLy <XtrayShow@yahoo.fr>
2024-11-24 14:35:35 +0100

    Modif de la table USER en UTILISATEUR

--- create.sql
+++ create.sql
@@ -1,11 +1,11 @@
 -- Supprimer les tables existantes
 DROP TABLE IF EXISTS COMMENT   CASCADE;
-DROP TABLE IF EXISTS TÂCHE     CASCADE;
+DROP TABLE IF EXISTS TACHE     CASCADE;
 DROP TABLE IF EXISTS CATEGORIE CASCADE;
-DROP TABLE IF EXISTS USER      CASCADE;
+DROP TABLE IF EXISTS UTILISATEUR CASCADE;
 
--- Création de la table USER
-CREATE TABLE USER (
+-- Création de la table UTILISATEUR
+CREATE TABLE UTILISATEUR (
 	id_user     SERIAL PRIMARY KEY,
 	email_user  VARCHAR(255) UNIQUE NOT NULL,
 	prenom_user VARCHAR(100) NOT NULL,
@@ -28,7 +28,7 @@ CREATE TABLE TACHE (
 	echeance_tache    DATE,
 	id_user           INT NOT NULL,
 	id_categorie      INT,
-	FOREIGN KEY (id_user)      REFERENCES USER(id_user) ON DELETE CASCADE,   -- Si un utilisateur est supprimé, ses tâches le sont aussi
+	FOREIGN KEY (id_user)      REFERENCES UTILISATEUR(id_user) ON DELETE CASCADE,   -- Si un utilisateur est supprimé, ses tâches le sont aussi
 	FOREIGN KEY (id_categorie) REFERENCES CATEGORIE(id_categorie) ON DELETE SET NULL -- Si une catégorie est supprimée, sa valeur est NULL dans TÂCHE
 );
 
@@ -37,5 +37,5 @@ CREATE TABLE COMMENT (
 	id_comment   SERIAL PRIMARY KEY,
 	text_comment TEXT NOT NULL,
 	id_tache     INT NOT NULL,
-	FOREIGN KEY (id_tache) REFERENCES TÂCHE(id_tache) ON DELETE CASCADE -- Si une tâche est supprimée, ses commentaires le sont aussi
+	FOREIGN KEY (id_tache) REFERENCES TACHE(id_tache) ON DELETE CASCADE -- Si une tâche est supprimée, ses commentaires le sont aussi
 );
