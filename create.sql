751dcfd3af8815d03552a21e10e5c31772c8bcf1
Kyllian LE BRETON <kyllian.le-breton@etu.univ-lehavre.fr>
2024-11-25 11:34:39 +0100

    Update CREATE - Reset Token

--- create.sql
+++ create.sql
@@ -10,7 +10,9 @@ CREATE TABLE UTILISATEUR (
 	email_user  VARCHAR(255) UNIQUE NOT NULL,
 	prenom_user VARCHAR(100) NOT NULL,
 	nom_user    VARCHAR(100) NOT NULL,
-	password    VARCHAR(255) NOT NULL
+	password    VARCHAR(255) NOT NULL,
+	reset_token VARCHAR(255),
+	reset_token_expiration TIMESTAMP
 );
 
 -- Création de la table CATEGORIE
