78f7a719ddcbcb89a0f09a1798a271a61f2bbdff
KillianDlds <killian.deslandes53@gmail.com>
2024-11-27 14:39:53 +0100

    categorie

--- app/Views/Tache/index.php
+++ app/Views/Tache/index.php
@@ -256,7 +256,7 @@ function switchView(view)
 						</div>
 						<div class="mb-3">
 							<label for="categorie" class="form-label">Catégorie</label>
-							<input type="text" class="form-control" name="categorie" id="categorie" list="categories-list" required>
+							<input type="text" class="form-control" name="categorie" id="categorie" list="categories-list">
 							<datalist id="categories-list">
 								<?php foreach ($categories as $categorie): ?>
 									<option value="<?= esc($categorie['titre_categorie']); ?>"></option>
