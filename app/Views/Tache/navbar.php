a376f394648c0c7a14e8e872a00f2149de5a67ec
capucine19 <capuc6ne@protonmail.com>
2024-11-28 13:16:02 +0100

    style navigation

--- app/Views/Tache/navbar.php
+++ app/Views/Tache/navbar.php
@@ -1,7 +1,7 @@
 <div class="wrapper">
     <div class="sidebar">
 
-        <div class="brand mb-4 mt-4">Bonjour, <span class="text-primary"><?= session()->get('prenom_user') ?></span></div>
+        <div class="brand mb-4 mt-4">Bonjour, <span><?= session()->get('prenom_user') ?></span></div>
         <nav class="nav flex-column">
             <a href="#tableau" class="bi bi-grid-1x2-fill nav-link active" id="viewTableau" onclick="switchView('tableau')">  Tableau</a>
             <a href="#tableur" class="bi bi-grid-3x2-gap-fill nav-link" id="viewTableur" onclick="switchView('tableur')">  Tableur</a>
