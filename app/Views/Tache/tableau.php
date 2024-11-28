bfcadd87fd86a18220a589f189f301f0ba28f045
capucine19 <capuc6ne@protonmail.com>
2024-11-28 12:49:43 +0100

    changement style navbar

--- app/Views/Tache/tableau.php
+++ app/Views/Tache/tableau.php
@@ -62,7 +62,7 @@ function renderTaskColumn($title, $tasks, $statusId, $modalTarget)
 							</div>
 							<div style="cursor: pointer;" data-bs-toggle="modal" data-bs-target="#taskDetailModal"
 								onclick="loadTaskDetails('<?= esc($task['titre']); ?>', '<?= esc($task['description_tache']); ?>', '<?= esc($task['echeance_tache']); ?>', '<?= esc($task['etat_tache']); ?>', '<?= esc($task['titre_categorie']); ?>', '<?= esc($task['id_tache']); ?>')">
-								<h3 class="t-title card-title mb-2"><?= esc($task['titre']); ?></h3>
+								<h4 class="t-title card-title mb-2"><?= esc($task['titre']); ?></h4>
 								<p class="card-text"><?= esc($task['description_tache']); ?></p>
 							</div>
 						</div>
