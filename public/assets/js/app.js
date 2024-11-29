586c94a4d107067dcfb501bfc089da5a6b1ea51b
Clementin Ly <XtrayShow@yahoo.fr>
2024-11-29 15:11:53 +0100

    Adieu Killian

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -37,11 +37,11 @@ function setupCharacterCounter()
 // Formater la date 
 function formatDate(dateString, includeTime = true) {
     const days = [
-        'dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'
+        'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'
     ];
     const months = [
-        'janvier', 'février', 'mars', 'avril', 'mai', 'juin', 
-        'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'
+        'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 
+        'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'
     ];
 
     const date = new Date(dateString);
@@ -67,12 +67,12 @@ function loadTaskDetails(titre, description, echeance, etat, categorie, id_tache
 {
 	console.log(titre, description, echeance, etat, categorie);
 
-    document.getElementById('detail_titre').innerText = titre;
+    document.getElementById('detail_titre').innerText       = titre;
     document.getElementById('detail_description').innerText = htmlEntityDecode(description);
-    document.getElementById('detail_echeance').innerText = echeance;
-    document.getElementById('detail_etat').innerText = etat;
-    document.getElementById('detail_categorie').innerText = categorie;
-    document.getElementById('comment_task_id').value = id_tache;
+    document.getElementById('detail_echeance').innerText    = formatDate(echeance, false);
+    document.getElementById('detail_etat').innerText        = etat;
+    document.getElementById('detail_categorie').innerText   = categorie;
+    document.getElementById('comment_task_id').value        = id_tache;
 
 	loadPaginatedComments(id_tache, 1); // Charger les commentaires de la première page
 
